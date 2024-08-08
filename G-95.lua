b87c04ad0db86eb810cf704622eeecab88377ec5
Zixsoed <rjjdksksosos@gmail.com>
2024-08-08 10:15:35 +0700

    Create G-95.lua

---
+++ G-95.lua
@@ -0,0 +1,13 @@
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+
+game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(255, 0, 0)
+game.Lighting.MainColorCorrection.Contrast = 1
+local tween = game:GetService("TweenService")
+tween:Create(game.Lighting.MainColorCorrection, TweenInfo.new(2.5), {Contrast = 0}):Play()
+local TweenService = game:GetService("TweenService")
+local TW = TweenService:Create(game.Lighting.MainColorCorrection, TweenInfo.new(30),{TintColor = Color3.fromRGB(255, 255, 255)})
+TW:Play()
+
+wait(2)
+
+loadstring(game:HttpGet("https://pastefy.app/oMcHkC5h/raw"))()
